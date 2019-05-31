c1872d73c35ebe1b9d6583f34c58d9703deba195
aifrs12 <santosafonso096@gmail.com>
2019-05-31 07:54:47 +0100

    ngif
    *ngIf="items.length > 0"
    *ngIf="items.length <= 0">Não existem leilões ativos.

--- src/app/items/item-list/item-list.component.ts
+++ src/app/items/item-list/item-list.component.ts
@@ -6,10 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./item-list.component.css']
 })
 export class ItemListComponent {
-  items = [
-    {title: 'Artigo numero 1', content: 'Este é o artigo numero 1'},
-    {title: 'Artigo numero 2', content: 'Este é o artigo numero 2'},
-    {title: 'Artigo numero 3', content: 'Este é o artigo numero 3'},
+ /* items = [
+    {title: 'Leilão numero 1', content: 'Este é o Leilão numero 1'},
+    {title: 'Leilão numero 2', content: 'Este é o Leilão numero 2'},
+    {title: 'Leilão numero 3', content: 'Este é o Leilão numero 3'},
   ]
+  */
+
+  items = [];
 
 }
