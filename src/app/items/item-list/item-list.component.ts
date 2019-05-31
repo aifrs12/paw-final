3b49dca6f73548c71ea7b6b96b03a957edaa97da
aifrs12 <santosafonso096@gmail.com>
2019-05-31 06:44:33 +0100

    e mais coisas

--- src/app/items/item-list/item-list.component.ts
+++ src/app/items/item-list/item-list.component.ts
@@ -2,8 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-item-list',
-  templateUrl: './item-list.component.html'
+  templateUrl: './item-list.component.html',
+  styleUrls: ['./item-list.component.css']
 })
 export class ItemListComponent {
+  items = [
+    {title: 'Artigo numero 1', content: 'Este é o artigo numero 1'},
+    {title: 'Artigo numero 2', content: 'Este é o artigo numero 2'},
+    {title: 'Artigo numero 3', content: 'Este é o artigo numero 3'},
+  ]
 
 }
