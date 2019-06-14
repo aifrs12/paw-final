41df95cd2c0d4b400d49b0971e98617e51a19e44
DanielCarvalho107 <37046651+DanielCarvalho107@users.noreply.github.com>
2019-06-14 14:12:10 +0100

    Revert "organised routes"
    This reverts commit d32c0402053add95352a840c1cb4e9b38ad22d09.

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-const itemsRoutes = require("./routes/items");
-
 const Item = require('./models/item');
 
 const app = express();
@@ -25,6 +23,54 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use("/api/items", itemsRoutes);
+app.post('/api/items/', (req, res, next) => {
+  const item = new Item({
+    title: req.body.title,
+    content: req.body.content
+  });
+  item.save().then(createdItem => {
+    res.status(201).json({
+      message: 'Leilão add sucess',
+      itemId: createdItem._id
+    });
+  });
+});
+
+app.put('/api/items/:id', (req, res, next) => {
+  const item = new Item({
+    _id: req.body.id,
+    title: req.body.title,
+    content: req.body.content
+  });
+  Item.updateOne({ _id: req.params.id }, item).then(result => {
+    res.status(200).json({ message: "Update successful!" });
+  });
+});
+
+app.get('/api/items/', (req, res, next) => {
+  Item.find().then(documents => {
+    res.status(200).json({
+      message: 'Leilões resposta sucess',
+      items: documents
+    });
+  });
+});
+
+app.get("/api/items/:id", (req, res, next) => {
+  Item.findById(req.params.id).then(item => {
+    if (item) {
+      res.status(200).json(item);
+    } else {
+      res.status(404).json({ message: "Item not found!" });
+    }
+  });
+});
+
+app.delete('/api/items/:id', (req, res, next) => {
+  Item.deleteOne({_id: req.params.id}).then(result => {
+    console.log(result);
+    res.status(200).json({ message: 'Leilão apagado' });
+  });
+});
 
 module.exports = app;
