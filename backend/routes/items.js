574c9cf14d1613a6e698d1d705d5c97f9787375d
aifrs12 <santosafonso096@gmail.com>
2019-06-19 14:59:05 +0100

    adding middleware checkpoint
    adding middleware checkpoint

--- backend/routes/items.js
+++ backend/routes/items.js
@@ -2,7 +2,9 @@ const express = require("express");
 const Item = require('../models/item');
 const router = express.Router();
 
-router.post('', (req, res, next) => {
+const checkAuth = require('../middleware/check-auth');
+
+router.post('', checkAuth, (req, res, next) => {
   const item = new Item({
     title: req.body.title,
     content: req.body.content
@@ -15,7 +17,7 @@ router.post('', (req, res, next) => {
   });
 });
 
-router.put('/:id', (req, res, next) => {
+router.put('/:id', checkAuth, (req, res, next) => {
   const item = new Item({
     _id: req.body.id,
     title: req.body.title,
@@ -54,7 +56,7 @@ router.get("/:id", (req, res, next) => {
   });
 });
 
-router.delete('/:id', (req, res, next) => {
+router.delete('/:id', checkAuth, (req, res, next) => {
   Item.deleteOne({_id: req.params.id}).then(result => {
     console.log(result);
     res.status(200).json({ message: 'Leilão apagado' });
