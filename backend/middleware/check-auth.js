3c47ba0b3303ecdc81908823fac979b4b39d34f3
aifrs12 <santosafonso096@gmail.com>
2019-06-19 23:24:06 +0100

    Update check-auth.js

--- backend/middleware/check-auth.js
+++ backend/middleware/check-auth.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
   try {
   const token = req.headers.authorization.split(' ')[1];
-    const decodedToken = jwt.verify(token, 'L^L^gdw?xc@WMZ&}8,A6c~sU4-');
+    jwt.verify(token, 'L^L^gdw?xc@WMZ&}8,A6c~sU4-');
     req.userData = {email: decodedToken.email, userId: decodedToken.userId};
     next();
 } catch (error) {
