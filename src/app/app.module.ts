5e2075f3e4589397bb363bbe1a9eb60cc1dad33d
aifrs12 <santosafonso096@gmail.com>
2019-06-19 22:00:57 +0100

    adding token to frontend
    Adding tokens to logins

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import {
   MatProgressSpinnerModule,
 } from '@angular/material';
 
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -21,6 +21,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { MatPaginatorModule } from '@angular/material';
+import { AuthInterceptor } from './auth/auth-interceptor';
 
 
 @NgModule({
@@ -48,7 +49,7 @@ import { MatPaginatorModule } from '@angular/material';
     MatProgressSpinnerModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [{provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { AuthData } from './signup/auth-data.model';
+import { AuthData } from './auth-data.model';
 
 @Injectable ({ providedIn: 'root' })
 export class AuthService {
+  private token: string;
+
+  getToken() {
+    return this.token;
+  }
+
   constructor(private http: HttpClient) {}
 
   createUser(email: string, password: string) {
@@ -16,9 +22,10 @@ export class AuthService {
 
   login(email: string, password: string) {
     const authData: AuthData = {email, password};
-    this.http.post('http://localhost:3000/api/user/login/', authData)
+    this.http.post<{token: string}>('http://localhost:3000/api/user/login/', authData)
     .subscribe(response => {
-      console.log(response);
+      const token = response.token;
+      this.token = token;
     });
   }
 }
