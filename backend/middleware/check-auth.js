1a75c29e078f334161f2842cc06009dfe98989cf
aifrs12 <santosafonso096@gmail.com>
2019-06-19 23:27:35 +0100

    just testes

--- backend/middleware/check-auth.js
+++ backend/middleware/check-auth.js
@@ -4,7 +4,6 @@ module.exports = (req, res, next) => {
   try {
   const token = req.headers.authorization.split(' ')[1];
     jwt.verify(token, 'L^L^gdw?xc@WMZ&}8,A6c~sU4-');
-    req.userData = {email: decodedToken.email, userId: decodedToken.userId};
     next();
 } catch (error) {
     res.status(401).json({message: 'Auth failed!'});
