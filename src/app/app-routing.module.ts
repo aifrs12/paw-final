b6615687ebf5e2c7817d317b859a6a386506a00e
aifrs12 <santosafonso096@gmail.com>
2019-06-19 22:51:56 +0100

    Adding AuthGuard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,17 +5,19 @@ import { ItemCreateComponent } from './items/item-create/item-create.component';
 import { ItemListComponent } from './items/item-list/item-list.component';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
+import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
   { path: '', component: ItemListComponent},
-  { path: 'create' , component: ItemCreateComponent},
-  { path: 'edit/:itemId' , component: ItemCreateComponent},
+  { path: 'create' , component: ItemCreateComponent, canActivate: [AuthGuard]},
+  { path: 'edit/:itemId' , component: ItemCreateComponent, canActivate: [AuthGuard]},
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [AuthGuard]
 })
 export class AppRoutingModule {}
