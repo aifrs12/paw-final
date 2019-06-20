738dadbef6f5321955be95c8387ccdfd171c2936
DanielCarvalho107 <37046651+DanielCarvalho107@users.noreply.github.com>
2019-06-20 17:30:00 +0100

    controls for image upload

--- src/app/items/item-create/item-create.component.ts
+++ src/app/items/item-create/item-create.component.ts
@@ -25,7 +25,8 @@ ngOnInit() {
     title: new FormControl(null, {
       validators: [Validators.required, Validators.minLength(3)]
     }),
-    content:  new FormControl(null, {validators: [Validators.required]})
+    content:  new FormControl(null, {validators: [Validators.required]}),
+    image: new FormControl(null, {validators: [Validators.required]})
   });
   this.route.paramMap.subscribe((paramMap: ParamMap) => {
     if (paramMap.has('itemId')) {
@@ -45,6 +46,12 @@ ngOnInit() {
         this.itemId = null;
     }
   });
+}
+
+onImagePicked(event: Event) {
+  const file = (event.target as HTMLInputElement).files[0];
+  this.form.patchValue({image: file});
+  this.form.get('image').updateValueAndValidity();
 }
   onSaveItem() {
     if (this.form.invalid) {
